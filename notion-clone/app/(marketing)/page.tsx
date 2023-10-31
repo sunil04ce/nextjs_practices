0c995692053ff9a191b393e1a7447153bb6c0150
sunil parmar <sunil04ce@gmail.com>
2023-10-31 23:48:59 +0530

    started marketing page, added favicon logos

--- notion-clone/app/(marketing)/page.tsx
+++ notion-clone/app/(marketing)/page.tsx
@@ -1,18 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Heading } from "./_components/heading";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <div>
-        <Button className="text-white bg-emerald-500" size="lg">
-          Success
-        </Button>
-      </div>
-      <div>
-        <Button variant="purple" size="lg">
-          Delete
-        </Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
       </div>
     </div>
   );
-}
+};
+
+export default MarketingPage;
