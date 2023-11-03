5653e1874cf9433a7582f66559797a3efd5de977
sunil parmar <sunil04ce@gmail.com>
2023-11-03 23:26:54 +0530

    added feature to change theme mode using dropdown

--- notion-clone/app/(marketing)/_components/navbar.tsx
+++ notion-clone/app/(marketing)/_components/navbar.tsx
@@ -1,24 +1,25 @@
 "use client";
 
 import useScrollTop from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
 
-const Navbar = () => {
+export const Navbar = () => {
   const scrolled = useScrollTop();
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full py-2 px-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login
+        <ModeToggle />
       </div>
     </div>
   );
 };
 
-export default Navbar;
+// export default Navbar;
