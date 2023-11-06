9e2b90c67b7e45d7f806726a80b5b0b5b82dc8d1
sunil parmar <sunil04ce@gmail.com>
2023-11-06 09:32:46 +0530

    changes in heading, navigated to documents

--- notion-clone/app/(marketing)/_components/navbar.tsx
+++ notion-clone/app/(marketing)/_components/navbar.tsx
@@ -6,9 +6,10 @@ import useScrollTop from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -33,6 +34,14 @@ export const Navbar = () => {
             <SignInButton mode="modal">
               <Button size="sm">Get Jotion Free</Button>
             </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
           </>
         )}
         <ModeToggle />
