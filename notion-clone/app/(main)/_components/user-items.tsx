459936b7961ec973488752312573e6eace941e08
sunil parmar <sunil04ce@gmail.com>
2023-11-16 23:15:33 +0530

    dropdown menu with email address

--- notion-clone/app/(main)/_components/user-items.tsx
+++ notion-clone/app/(main)/_components/user-items.tsx
@@ -14,7 +14,38 @@ import { useUser } from "@clerk/clerk-react";
 
 const UserItem = () => {
   const { user } = useUser();
-  return <div>UserItem</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+              <span className="text-start font-medium line-clamp-1">
+                {user?.fullName}&apos;s Jotion
+              </span>
+            </Avatar>
+          </div>
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-80"
+        align="start"
+        alignOffset={11}
+        forceMount
+      >
+        <div className="flex flex-col space-y-4 p-2">
+          <p className="text-xs font-medium leading-none text-muted-foreground">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserItem;
