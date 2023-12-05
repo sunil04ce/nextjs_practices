cbd272a21ad720529cfe35b6bfa99bce7a0d82ab
sunil parmar <sunil04ce@gmail.com>
2023-12-05 23:13:34 +0530

    displayed add icon, add cover buttons

--- notion-clone/components/toolbar.tsx
+++ notion-clone/components/toolbar.tsx
@@ -2,6 +2,8 @@
 
 import { Doc } from "@/convex/_generated/dataModel";
 import IconPicker from "./icon-picker";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile, X } from "lucide-react";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -18,8 +20,44 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
               {initialData.icon}
             </p>
           </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
         </div>
       )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+      <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add cover
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
