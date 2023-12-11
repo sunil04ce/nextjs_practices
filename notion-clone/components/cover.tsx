b5fc3d74025d50bcdb45c0ea7068a94dc0f6dcd3
sunil parmar <sunil04ce@gmail.com>
2023-12-11 22:42:44 +0530

    remove and replace cover image

--- notion-clone/components/cover.tsx
+++ notion-clone/components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -16,11 +17,18 @@ interface CoverImageProps {
 }
 
 const Cover = ({ url, preview }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverIcon);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
+
     removeCoverImage({
       id: params.documentId as Id<"documents">,
     });
@@ -38,13 +46,13 @@ const Cover = ({ url, preview }: CoverImageProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
           >
             <ImageIcon className="h-4 w-4 mr-2" />
-            Cover Image
+            Change Image
           </Button>
           <Button
             onClick={onRemove}
